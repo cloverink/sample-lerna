238edf2204bf202dcddc26d34327fa30734076fe
Aran(Tae) Chananar <aran.c@pomelofashion.com>
2022-06-22 17:02:29 +0700

    feat: hide foo

--- packages/helloworld/src/index.ts
+++ packages/helloworld/src/index.ts
@@ -1,4 +1,6 @@
 import Hello from './components/hello';
-import Foo from './components/foo';
+// import Foo from './components/foo';
 
-export { Hello, Foo }
+// export { Hello, Foo }
+
+export { Hello }
