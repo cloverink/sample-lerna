3746edd6486597721060a1a3232f3fedd6e36788
Aran(Tae) Chananar <aran.c@pomelofashion.com>
2022-06-23 00:47:00 +0700

    feat: force publish

--- release.config.js
+++ release.config.js
@@ -6,7 +6,12 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/github',
-    '@semantic-release/npm',
+    [
+      "@semantic-release/npm",
+      {
+        "npmPublish": false
+      }
+    ],
     [
       '@semantic-release/git',
       {
