dc1836bdbc71a760303b8f79aa1990e2c55cf008
Aran(Tae) Chananar <aran.c@pomelofashion.com>
2022-06-27 17:03:02 +0700

    feat: test release

--- release.config.js
+++ release.config.js
@@ -3,23 +3,22 @@ module.exports = {
   dryRun: false,
   plugins: [
     '@semantic-release/commit-analyzer',
-    'semantic-release-lerna',
+    ["semantic-release-lerna",
+      {
+        "generateNotes": false
+      }
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/github',
-    [
-      "@semantic-release/npm",
-      {
-        "npmPublish": false
-      }
-    ],
+    "@semantic-release/npm",
     [
       '@semantic-release/git',
       {
         assets: [
           'docs/CHANGELOG.md',
           "package.json",
-          "packages/*/package.json"
+          "package-lock.json"
         ],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
